20ff45c8364737ac8d23ed1bf33ce33e3e40e165
caofangsheng93 <fangsheng93@163.com>
2016-12-09 15:52:46 +0800

    ...

--- ExportToExcel/Models/StaticDataOfStudent.cs
+++ ExportToExcel/Models/StaticDataOfStudent.cs
@@ -16,8 +16,8 @@ namespace ExportToExcel.Models
             {
                 return new List<Student>() 
                 {
-                new Student(){ID=1,Name="曹方胜",Sex="男",Email="fangsheng93@163.com",Age=24},
-                new Student(){ID=2,Name="李灵婕",Sex="女",Email="lilingjie@sina.com.cn",Age=24},
+                new Student(){ID=1,Name="曹操",Sex="男",Email="caocao@163.com",Age=24},
+                new Student(){ID=2,Name="李易峰",Sex="女",Email="lilingjie@sina.com.cn",Age=24},
                 new Student(){ID=3,Name="张三丰",Sex="男",Email="zhangsanfeng@qq.com",Age=224},
                 new Student(){ID=4,Name="孙权",Sex="男",Email="sunquan@163.com",Age=1224},
                 };
